a0ffc9c8a78788fbb7fb59885ac07cecced08a65
reusoftware <105401707+reusoftware@users.noreply.github.com>
2024-06-05 20:06:04 +0800

    app.js

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ document.getElementById('disconnectButton').addEventListener('click', () => {
     }
 });
 
-document.getElementById('joinRoomButton').addEventListener('click', async () => {
-    const room = document.getElementById('room').value;
-    await joinRoom(room);
-});
+joinRoomButton.addEventListener('click', async () => {
+        const room = document.getElementById('room').value;
+        await joinRoom(room);
+    });
 
 document.getElementById('sendMessageButton').addEventListener('click', () => {
     const message = document.getElementById('messageInput').value;
